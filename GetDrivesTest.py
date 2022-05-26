7d8eeca001a030aca7b5070891d99c82d8d5da15
DannoPeters <Danno306@gmail.com>
2022-05-26 17:07:34 -0600

    Get drives on Mac, untested on Windows and Linux

--- GetDrivesTest.py
+++ GetDrivesTest.py
@@ -1,12 +1,18 @@
-import pyudev
-context = pyudev.Context()
-monitor = pyudev.Monitor.from_netlink()
-# For USB devices
-monitor.filter_by(susbsytem='usb')
-# OR specifically for most USB serial devices
-monitor.filter_by(susbystem='tty')
-for action, device in monitor:
-    vendor_id = device.get('ID_VENDOR_ID')
-    # I know the devices I am looking for have a vendor ID of '22fa'
-    if vendor_id in ['22fa']:
-        print('Detected {} for device with vendor ID {}'.format(action, vendor_id))
+import os
+import platform
+
+#Check OS of User
+operatingSystem = platform.system()
+
+if operatingSystem == 'Darwin': #Mac
+    drives = os.listdir('/Volumes')
+    print(drives)
+
+if operatingSystem == 'Linux': #Linux
+    print(os.listdir('/Volumes'))
+
+if operatingSystem == 'Windows': #Windows
+    import os.path
+    dl = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    drives = ['%s:' % d for d in dl if os.path.exists('%s:' % d)]
+    print(drives)
